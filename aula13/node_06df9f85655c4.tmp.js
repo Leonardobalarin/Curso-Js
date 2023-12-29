c0cb94a87d6160c7d1fe79da2ac3933cccb59b75
LeonardoBalarine <145723834+Leonardobalarin@users.noreply.github.com>
2023-12-29 20:40:52 -0300

    aula 13

---
+++ aula13/node_06df9f85655c4.tmp.js
@@ -0,0 +1,25 @@
+let c = 1
+do {
+    console.log(`Passo ${c}`);
+    c--
+} while (c <= 10)
+
+
+
+/*
+let c = 1
+while (c < 10) {
+    console.log(`Passo ${c}`);
+    c++
+}
+*/
+
+
+/*
+console.log('Tudo bem');
+console.log('Tudo bem');
+console.log('Tudo bem');
+console.log('Tudo bem');
+console.log('Tudo bem');
+console.log('Tudo bem');
+*/
