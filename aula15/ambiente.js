a75d2acef8afbe08123adab244d455b4a6b45a98
LeonardoBalarine <145723834+Leonardobalarin@users.noreply.github.com>
2024-01-03 21:40:29 -0300

    aula 15

---
+++ aula15/ambiente.js
@@ -0,0 +1,6 @@
+let num = [1, 2, 3, 4, 5]
+num[5] = 6 // adicionar mais espaço
+num.push(7) // adicionar espaço a ultima posição
+// num.length (saber o comprimento)
+// num.sort() (coloca os vetores em ordem)
+console.log(`Nosso vetor é o ${num}`);
